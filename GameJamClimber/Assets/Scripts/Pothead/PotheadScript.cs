5e9db116a4af1e88fcd5ecb50bcad849f6ea3c5c
Nichi144 <ashwin.seetharaman144@gmail.com>
2025-04-12 15:05:21 -0400

    Animations

--- GameJamClimber/Assets/Scripts/Pothead/PotheadScript.cs
+++ GameJamClimber/Assets/Scripts/Pothead/PotheadScript.cs
@@ -11,6 +11,8 @@ public class PotheadScript : MonoBehaviour
     public bool is_jump;
     public bool is_grounded;
     InputActions input;
+    private Animator animator;
+    SpriteRenderer spriteRenderer;
 
     void Awake()
     {
@@ -21,14 +23,25 @@ public class PotheadScript : MonoBehaviour
     void Start()
     {
         PotHeadBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal");
+        animator.SetBool("Run", horizontalMove != 0);
+        animator.SetBool("JumpEnd", is_grounded);
+        if (horizontalMove < 0){
+            spriteRenderer.flipX = true;
+        }
+        if (horizontalMove > 0){
+            spriteRenderer.flipX = false;
+        }
+        animator.SetFloat("VerticalMove", PotHeadBody.velocity.y);
         if (is_jump && is_grounded){
-            PotHeadBody.AddForce(Vector2.up * 10f);
+            PotHeadBody.AddForce(Vector2.up * 50f);
             StartCoroutine(Wait());
             // is_jump = false;
         }
